7e6e5c251903c116f9ff7aa8900655eb2fddd7e5
tufy <tufyelias24@gmail.com>
2024-05-21 19:58:34 -0300

    atualizações finais

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        
+
+    Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Digite seu numero de usuario:");
+      int numero = scanner.nextInt();
+
+      System.out.println("Digite o numero da sua agencia:");
+      String agencia = scanner.next();
+
+      System.out.println("Digite seu nome:");
+      String nome = scanner.next();
+
+      System.out.println("Digite seu saldo:");
+      Double saldo = scanner.nextDouble();
+
+      System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
